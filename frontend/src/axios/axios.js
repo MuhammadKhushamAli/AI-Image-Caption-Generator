34add704427efb6b5893496ef99fc9b114006808
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2025-11-14 23:43:54 +0500

    Axios Tokens Regeneration Added

--- frontend/src/axios/axios.js
+++ frontend/src/axios/axios.js
@@ -8,3 +8,21 @@ export const axiosInstance = axios.create({
   },
   timeout: 10000,
 });
+
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const orignalRequest = error.config;
+    if (error.response?.status === 401 && !orignalRequest._retry) {
+      orignalRequest._retry = true;
+      try {
+        await axiosInstance.get("/api/v1/users/refresh-tokens");
+        return axiosInstance(orignalRequest);
+      } catch (error) {
+        window.location.href = "/login";
+        return Promise.reject(error);
+      }
+    }
+    return Promise.reject(error);
+  }
+);
