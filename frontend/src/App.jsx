4e8cbf1a107ef3b3f1376c929349a72aa3509cff
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2025-11-14 22:59:39 +0500

    App.jsx Configed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,32 @@
 import { Outlet } from "react-router";
 import { Container, Header } from "./components";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { axiosInstance } from "./axios/axios.js";
+import { login } from "./features/authentication/authSlice.js;
+import { Error } from "./components/Error.jsx";
 
 function App() {
+  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    setError('');
+    try {
+      const currentUserResponse = await axiosInstance.get("/api/v1/users/current-user");
+      
+      if(currentUserResponse.statusCode === 200)
+      {
+        dispatch(login({userData: currentUserResponse?.data}));
+      }
+      
+    } catch (error) {
+      setError(error);
+    }
+  }, [])
   return (
     <Container>
+      {error && <Error error={error} />}
       <Header />
       <Outlet />
     </Container>
