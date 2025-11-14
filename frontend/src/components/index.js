3a25bee35ea3aecd1e7febf5a83771c0f3f23aa0
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2025-11-15 00:11:02 +0500

    Remane AuthLAyout And Added to index

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -5,5 +5,15 @@ import { SignUp } from "./signup/SIgnUp.jsx";
 import { Home } from "./home/Home.jsx";
 import { History } from "./history/History.jsx";
 import { ChatCard } from "./chatCard/ChatCard.jsx";
+import { AuthLayout } from "./AuthLayout.jsx";
 
-export { Container, Header, Login, SignUp, Home, History, ChatCard };
+export {
+  Container,
+  Header,
+  Login,
+  SignUp,
+  Home,
+  History,
+  ChatCard,
+  AuthLayout,
+};
