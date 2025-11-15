fa3ebe0f64a31934aa6cb8b9ebfa4894da5e5382
Muhammad Khusham Ali <muhammadkhushamali79@gmail.com>
2025-11-15 16:44:08 +0500

    Redis Configed

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
 import app from "./app.js";
+import { client } from "./redis";
 
 dotenv.config({
   path: "./.env",
@@ -8,6 +9,7 @@ dotenv.config({
 
 connectDB()
   .then(() => {
+    client.connect();
     app.listen(process.env.PORT || 4000, () => {
       console.log(`Server is Running on ${process.env.PORT}`);
     });
